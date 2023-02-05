4450083d5414aa033289a864dbc184629b018b31
Isaac Quidute <isaacquidutemelo13@gmail.com>
2023-02-05 14:25:55 -0300

    feat: initial structure

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome!')
+// Using EJS as View Engine
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => { 
+    res.render('index');
 });
 
 app.listen(8080, () => {
